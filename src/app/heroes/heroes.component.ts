0eec0f4c3681945396350732357f2f304eb16224
cheatskill <sumi_y@itc-tokyo.com>
2023-03-25 01:47:05 +0900

    HeroesComponentの更新

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,14 +9,25 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent {
+
   hero : Hero = {
     id: 1,
     name: 'Windstorm'
   };
-  heroes = HEROES;
+  heroes :Hero[] = [];
   selectedHero?: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
